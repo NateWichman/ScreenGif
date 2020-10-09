af280c32f35b4049b78836c34266b361df11aedb
Nathan Wichman <nwichman@ostusa.com>
2020-10-09 12:53:50 -0400

    can finally convert to gif

--- main.js
+++ main.js
@@ -1,86 +1,107 @@
-const {app, BrowserWindow, dialog} = require('electron')
-const url = require("url");
-const path = require("path");
+const { app, BrowserWindow, dialog } = require('electron');
+const url = require('url');
+const path = require('path');
 const fs = require('fs');
 const electron = require('electron');
 const ipc = electron.ipcMain;
 
-let mainWindow
+let mainWindow;
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/ScreenGif/index.html`),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/ScreenGif/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 }
-
-let overlayWindow;
-ipc.on('openOverlay', (event) => {
-  overlayWindow = new BrowserWindow({
-    fullscreen: true,
-    movable: false,
-    transparent: true,
-    frame: false,
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  overlayWindow.setAlwaysOnTop(true, 'pop-up-menu');
-
-  overlayWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/ScreenGif/index.html`),
-      hash: '/overlay',
-      protocol: "file:",
-      slashes: true
-    })
-  );
-  overlayWindow.show();
-});
-ipc.on('cancelOverlay', (event) => {
-  overlayWindow.close();
-})
-
-ipc.on('selectOverlay', (event, clipPercents) => {
-  mainWindow.webContents.send('clip', clipPercents);
-});
-
-
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', function () {
-  if (mainWindow === null) createWindow()
-})
-
-ipc.on('saveFile', async function (event, blob) {
-  const { filePath } = await dialog.showSaveDialog({
-    buttonLabel: 'Save Video',
-    defaultPath: `vid-${Date.now()}.webm`
-  });
-  if (filePath) {
-    fs.writeFile(filePath, blob, () => console.log('complete'));
-  }
+    if (mainWindow === null) createWindow();
 });
+
+/** OVERLAY */
+let overlayWindow;
+ipc.on('openOverlay', (event) => {
+    console.log('overlay opened');
+    overlayWindow = new BrowserWindow({
+        fullscreen: true,
+        movable: false,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    overlayWindow.setAlwaysOnTop(true, 'pop-up-menu');
+
+    overlayWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/ScreenGif/index.html`),
+            hash: '/overlay',
+            protocol: 'file:',
+            slashes: true
+        })
+    );
+    overlayWindow.show();
+});
+
+ipc.on('cancelOverlay', (event) => {
+    overlayWindow.close();
+});
+
+ipc.on('selectOverlay', (event, clipPercents) => {
+    mainWindow.webContents.send('clip', clipPercents);
+});
+
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('ffmpeg-static').replace(
+    'app.asar',
+    'app.asar.unpacked'
+);
+const ffprobePath = require('ffprobe-static').path.replace(
+    'app.asar',
+    'app.asar.unpacked'
+);
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+/** File Saving */
+ipc.on('saveFile', async function (event, blob) {
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Save Video',
+        defaultPath: `vid-${Date.now()}.webm`
+    });
+    if (filePath) {
+        await fs.writeFile(filePath, blob, () => console.log('complete'));
+
+        const outputPath = filePath.replace('webm', 'gif');
+        console.log('\x1b[36m%s\x1b[0m', 'OUTPUT PATHHHH', outputPath);
+
+        ffmpeg(filePath)
+            .outputOption('-vf', 'fps=20')
+            .save(outputPath);
+    }
+});
+
+/** GIF conversion */
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,24 +1,36 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    Input,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+    selector: 'app-editor',
+    templateUrl: './editor.component.html',
+    styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements AfterViewInit {
-  @ViewChild('vid') videoEl: ElementRef;
-  @Input() record: Blob;
-  videoUrl;
+    @ViewChild('vid') videoEl: ElementRef;
+    @Input() record: any;
+    videoUrl;
 
-  constructor(private cd: ChangeDetectorRef) { }
+    constructor(private cd: ChangeDetectorRef, private eS: ElectronService) {}
 
-  ngAfterViewInit() {
-    console.log('record', this.record)
-    this.videoUrl = window.URL.createObjectURL(this.record);
-    this.videoEl.nativeElement.src = this.videoUrl;
-    this.cd.detectChanges();
-    this.videoEl.nativeElement.play();
-  }
+    ngAfterViewInit() {
+        console.log('record', this.record);
+        this.videoUrl = window.URL.createObjectURL(this.record);
+        this.videoEl.nativeElement.src = this.videoUrl;
+        this.cd.detectChanges();
+        this.videoEl.nativeElement.play();
+    }
 
-
+    async save() {
+        const buffer = Buffer.from(await this.record.arrayBuffer());
+        this.eS.ipcRenderer.send('saveFile', buffer);
+    }
 }
